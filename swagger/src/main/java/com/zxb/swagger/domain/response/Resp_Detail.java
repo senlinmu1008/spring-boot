04a7090b8c40ebd3a089899c0b755e865ed93d8f
senlinmu1008 <13486627323@163.com>
2021-01-14 22:40:38 +0800

    NotBlank注解改包

--- swagger/src/main/java/com/zxb/swagger/domain/response/Resp_Detail.java
+++ swagger/src/main/java/com/zxb/swagger/domain/response/Resp_Detail.java
@@ -13,7 +13,8 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  *
