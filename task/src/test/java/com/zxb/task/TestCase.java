1029d9228f98559233105b37793fba4034f750a9
zhaoxb <13486627323@163.com>
2021-03-25 18:13:28 +0800

    验证当前日期是否符合cron表达式

--- task/src/test/java/com/zxb/task/TestCase.java
+++ task/src/test/java/com/zxb/task/TestCase.java
@@ -3,8 +3,11 @@
  */
 package com.zxb.task;
 
+import lombok.SneakyThrows;
 import org.junit.Test;
 import org.quartz.CronExpression;
+
+import java.util.Date;
 
 /**
  *
@@ -16,5 +19,13 @@ public class TestCase {
     public void testCron() {
         String cron = "*/5 * * * * ?";
         System.out.println(CronExpression.isValidExpression(cron));
+    }
+
+    @Test
+    @SneakyThrows
+    public void testResCron() {
+        CronExpression cronExpression = new CronExpression("* */5 7-23 * * ?");
+        boolean resCron = cronExpression.isSatisfiedBy(new Date());
+        System.out.println(resCron);
     }
 }
