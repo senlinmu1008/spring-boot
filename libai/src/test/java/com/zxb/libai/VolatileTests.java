24b3530c49c571a0500b30e32ee22f7324b01563
zhaoxb <13486627323@163.com>
2021-05-19 23:43:11 +0800

    volatile指令重排序测试

--- libai/src/test/java/com/zxb/libai/VolatileTests.java
+++ libai/src/test/java/com/zxb/libai/VolatileTests.java
@@ -1,18 +1,21 @@
 package com.zxb.libai;
 
 import cn.hutool.core.thread.ThreadUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 /**
  * @author zhaoxb
  * @date 2021-05-09 9:07 下午
  */
+@Slf4j
 public class VolatileTests {
-    private volatile boolean flag = true;
 
     /**
      * 测试可见性
      */
+    private boolean flag = true;
+//    private volatile boolean flag = true;
     @Test
     public void test1() throws InterruptedException {
         Thread t = new Thread(() -> {
@@ -31,10 +34,39 @@ public class VolatileTests {
     /**
      * 测试指令重排序
      */
+//    private int x, y, a, b;
+    private volatile int x, y, a, b;
     @Test
-    public void test2() {
+    public void test2() throws InterruptedException {
         for(int n = 1;;n++) {
-
+            x = 0; y = 0;
+            a = 0; b = 0;
+            Thread t1 = new Thread(() -> {
+                a = 1; // 1
+                x = b; // 2
+            });
+            Thread t2 = new Thread(() -> {
+                b = 1; // 3
+                y = a; // 4
+            });
+            // 启动2个线程
+            t1.start(); t2.start();
+            // 等这2个线程都执行完毕后再输出x、y的值
+            t1.join(); t2.join();
+            /*
+            假设不出现指令重排序，则会出现以下几种排列组合
+            1 2 3 4 x = 0 y = 1
+            1 3 4 2 x = 1 y = 1
+            1 3 2 4 x = 1 y = 1
+            3 1 2 4 x = 1 y = 1
+            3 4 1 2 x = 1 y = 0
+            3 1 4 2 x = 1 y = 1
+            如果出现 x = 0 && y = 0这种情况，则可以反证一定出现了重排序
+             */
+            if (x == 0 && y == 0) {
+                log.info("第{}次出现了x = 0、Y = 0的情况", n);
+                break;
+            }
         }
     }
 }
