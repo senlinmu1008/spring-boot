ebc28f8c6647a4c2dd31e2efc5854b1d8552fb54
zhaoxb <13486627323@163.com>
2024-01-03 13:31:43 +0800

    socket读取配置关闭流优化

--- socket-action/src/main/java/net/zhaoxiaobin/socket/config/SocketChannelConfigParse.java
+++ socket-action/src/main/java/net/zhaoxiaobin/socket/config/SocketChannelConfigParse.java
@@ -1,5 +1,6 @@
 package net.zhaoxiaobin.socket.config;
 
+import cn.hutool.core.io.IoUtil;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import net.zhaoxiaobin.socket.config.domain.SocketChannelConfig;
 import net.zhaoxiaobin.socket.config.domain.SocketInConfigProperties;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StreamUtils;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,33 +39,39 @@ public class SocketChannelConfigParse implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         logger.info("开始解析socket接入渠道配置文件");
         for (Resource resource : configurations) {
-            String xmlContent = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-            if (StringUtils.isBlank(xmlContent)) {
-                continue;
+            InputStream inputStream = null;
+            try {
+                inputStream = resource.getInputStream();
+                String xmlContent = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+                if (StringUtils.isBlank(xmlContent)) {
+                    continue;
+                }
+                XmlMapper mapper = new XmlMapper();
+                // 如果xml中有节点，但实体类中没有属性对应，不报错处理
+                // mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                List<SocketChannelConfig> socketChannelConfigList = mapper.readValue(xmlContent, SocketInConfigProperties.class).getSocketChannelConfigList();
+                if (socketChannelConfigList == null) {
+                    continue;
+                }
+                // 校验必填字段
+                socketChannelConfigList.forEach(socketChannelConfig -> {
+                    if (socketChannelConfig.getPort() == 0) {
+                        logger.error("配置文件:{}接入渠道端口没有配置", resource.getFilename());
+                        throw new RuntimeException("配置文件:" + resource.getFilename() + "接入渠道端口没有配置");
+                    }
+                    if (StringUtils.isBlank(socketChannelConfig.getBeanName())) {
+                        logger.error("配置文件:{}接入渠道beanName没有配置", resource.getFilename());
+                        throw new RuntimeException("配置文件:" + resource.getFilename() + "接入渠道beanName没有配置");
+                    }
+                    if (socketChannelConfig.getMaxConcurrency() <= 0) {
+                        logger.warn("端口:{}渠道设置的最大并发数:{}不合理,重新设为:{}", socketChannelConfig.getPort(), socketChannelConfig.getMaxConcurrency(), MAX_CONCURRENCY);
+                        socketChannelConfig.setMaxConcurrency(MAX_CONCURRENCY);
+                    }
+                });
+                this.socketChannelConfigList.addAll(socketChannelConfigList);
+            } finally {
+                IoUtil.close(inputStream);
             }
-            XmlMapper mapper = new XmlMapper();
-            // 如果xml中有节点，但实体类中没有属性对应，不报错处理
-//            mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-            List<SocketChannelConfig> socketChannelConfigList = mapper.readValue(xmlContent, SocketInConfigProperties.class).getSocketChannelConfigList();
-            if (socketChannelConfigList == null) {
-                continue;
-            }
-            // 校验必填字段
-            socketChannelConfigList.forEach(socketChannelConfig -> {
-                if (socketChannelConfig.getPort() == 0) {
-                    logger.error("配置文件:{}接入渠道端口没有配置", resource.getFilename());
-                    throw new RuntimeException("配置文件:" + resource.getFilename() + "接入渠道端口没有配置");
-                }
-                if (StringUtils.isBlank(socketChannelConfig.getBeanName())) {
-                    logger.error("配置文件:{}接入渠道beanName没有配置", resource.getFilename());
-                    throw new RuntimeException("配置文件:" + resource.getFilename() + "接入渠道beanName没有配置");
-                }
-                if (socketChannelConfig.getMaxConcurrency() <= 0) {
-                    logger.warn("端口:{}渠道设置的最大并发数:{}不合理,重新设为:{}", socketChannelConfig.getPort(), socketChannelConfig.getMaxConcurrency(), MAX_CONCURRENCY);
-                    socketChannelConfig.setMaxConcurrency(MAX_CONCURRENCY);
-                }
-            });
-            this.socketChannelConfigList.addAll(socketChannelConfigList);
         }
     }
 
